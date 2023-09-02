c3034f5ef0a89ae19fc932406dccd1a33383120c
ou5ama <lasri.oussama@gmail.com>
2023-09-02 11:56:49 +0100

    define and setup the UserRepository

---
+++ src/main/java/com/example/jwt_security/dao/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.jwt_security.dao;
+
+import com.example.jwt_security.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
