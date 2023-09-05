00053e2cd225c72a8ecc56e5060f1b0795214612
ou5ama <lasri.oussama@gmail.com>
2023-09-05 23:13:32 +0100

    add AuthenticationManager Bean the manage the auth

--- src/main/java/com/example/jwt_security/config/ApplicationConfig.java
+++ src/main/java/com/example/jwt_security/config/ApplicationConfig.java
@@ -4,8 +4,10 @@ import com.example.jwt_security.dao.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,6 +32,12 @@ public class ApplicationConfig {
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
 //    Create a password Encoder
     @Bean
     public PasswordEncoder passwordEncoder() {
