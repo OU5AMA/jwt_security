b88b4e1d4f39a51587a25b43ae25ea6925a6ec69
ou5ama <lasri.oussama@gmail.com>
2023-09-05 23:27:11 +0100

    add two endpoints register, and authenticate

--- src/main/java/com/example/jwt_security/auth/AuthenticationController.java
+++ src/main/java/com/example/jwt_security/auth/AuthenticationController.java
@@ -1,6 +1,9 @@
 package com.example.jwt_security.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ){
+//        empty for now, will implement it later
+    }
 
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody AuthenticateRequest request
+    ){
+//        empty for now, will implement it later
+    }
 }
