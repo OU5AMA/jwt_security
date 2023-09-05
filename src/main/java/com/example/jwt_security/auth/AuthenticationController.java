bd8fb3e84f4ec8d79c10dbd628d0b11c77eac264
ou5ama <lasri.oussama@gmail.com>
2023-09-06 00:54:31 +0100

    implement the register

--- src/main/java/com/example/jwt_security/auth/AuthenticationController.java
+++ src/main/java/com/example/jwt_security/auth/AuthenticationController.java
@@ -1,5 +1,6 @@
 package com.example.jwt_security.auth;
 
+import com.example.jwt_security.auth.service.AuthenticateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,17 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    private final AuthenticateService authService;
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ){
-//        empty for now, will implement it later
+      return ResponseEntity.ok(authService.register(request))
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticateRequest request
     ){
-//        empty for now, will implement it later
+        return ResponseEntity.ok(authService.Autheticate(request))
     }
 }
---
+++ src/main/java/com/example/jwt_security/auth/service/AuthenticateService.java
@@ -0,0 +1,40 @@
+package com.example.jwt_security.auth.service;
+
+import com.example.jwt_security.auth.AuthenticateRequest;
+import com.example.jwt_security.auth.AuthenticationResponse;
+import com.example.jwt_security.auth.RegisterRequest;
+import com.example.jwt_security.config.JwtService;
+import com.example.jwt_security.dao.UserRepository;
+import com.example.jwt_security.user.Role;
+import com.example.jwt_security.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticateService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse Autheticate(AuthenticateRequest request) {
+    }
+}
+
+
